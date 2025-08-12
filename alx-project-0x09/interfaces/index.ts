bf0d867fbf8f1ee7881668b69f3cb8df7bbca99b
Ogidol <ogundele115@gmail.com>
2025-08-12 14:41:39 +0100

    first commit

--- alx-project-0x09/interfaces/index.ts
+++ alx-project-0x09/interfaces/index.ts
@@ -1,21 +1,3 @@
-export interface PropertyProps {
-  name: string;
-  description: string;
-  address: { state: string; city: string; country: string };
-  rating: number;
-  category: string[]; // tags like "Pool", "Self Checkin"
-  price: number;
-  offers: { bed: string; shower: string; occupants: string };
-  image: string;
-  discount?: string;
-}
-
-import * as React from "react";
-
-export interface ReactComponentProps {
-  children: React.ReactNode;
-}
-
 import { ReactNode } from "react";
 
 export interface ReactComponentProps {
@@ -29,5 +11,9 @@ export interface GeneratedImageProps {
   height?: string;
   action: (imagePath: string) => void;
 }
+
+export type RequestProps = {
+  prompt: string;
+};
 
 export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">;
